66c673b7f2f96d0a4574c58c3e9aeb8e28baa5e9
GuotongWu <wuguotongswjtu@gmail.com>
2021-10-26 23:00:08 +0800

    a little modify

--- padding.py
+++ padding.py
@@ -4,6 +4,7 @@ import librosa
 import matplotlib.pyplot as plt
 import pickle as pkl
 from librosa.feature.spectral import mfcc
+from mpl_toolkits.mplot3d import Axes3D
 
 def padding(audio_data, padding_len=291500):
     padding_data = []
@@ -32,8 +33,23 @@ def read_audio():
 def save_obj(obj, name):
     with open(name+'.pkl', 'wb') as f:
         pkl.dump(obj, f, pkl.HIGHEST_PROTOCOL)
- 
 
+
+def draw_feature(audio_data):
+    fig = plt.figure() 
+    ax = Axes3D(fig)
+    
+    xy = [[],[]]
+    for i in range(50):
+        xy[0] += [i]*20
+    xy[1] = list(range(20))*50
+
+    # print(audio_data['feature'][0][:, :100].reshape((-1,)))
+    ax.scatter3D(xy[0], xy[1], audio_data['feature'][0][:, 270:320].reshape((-1,)))
+    plt.show()
+
+ 
 if __name__ == '__main__':
     audio_data = read_audio()
     save_obj(audio_data, './src/audio')
+    draw_feature(audio_data)
